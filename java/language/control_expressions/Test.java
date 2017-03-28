a6aa244ea8932059d5f7f1209d350acfc96b5892
Billy Murphy <billy.1.murphy@gmail.com>
2017-03-27 19:13:52 -0500

    Cleaning ...

--- java/language/control_expressions/Test.java
+++ java/language/control_expressions/Test.java
@@ -1,51 +1,59 @@
 class Test {
+
+/**
+
+	SIMPLE LOGIC GATES
+
+**/
+
 static final String REQUEST_ATTRIBUTE = null;
 
-public static void main(String args[]){
+	public static void main(String args[]){
 
-	boolean bpaddepot = true;
-	boolean pseudo = true;
-	boolean pratt = false;
-	boolean martin = false;
-//	if (  bpaddepot && pseudo || pratt || martin  ){
-//		System.out.println("Gate Succesful");
-//	}
+		boolean bpaddepot = true;
+		boolean pseudo = true;
+		boolean pratt = false;
+		boolean martin = false;
 
-			if ( ( false ) &&  ( true )
+	//	if (  bpaddepot && pseudo || pratt || martin  ){
+	//		System.out.println("Gate Succesful");
+	//	}
+
+	if ( ( false ) &&  ( true ) ) {}
 
 
-//ClientConstants.APPNAME_VALUE.equalsIgnoreCase("bp") && Util.stringTest(ClientConstants.PSEUDO_APPNAME) || ClientConstants.CLIENTNAME_VALUE.equalsIgnoreCase("PRATT")
+	//ClientConstants.APPNAME_VALUE.equalsIgnoreCase("bp") && Util.stringTest(ClientConstants.PSEUDO_APPNAME) || ClientConstants.CLIENTNAME_VALUE.equalsIgnoreCase("PRATT")
 
-/*	boolean see = true;
-	boolean learn = true;
+	/*	boolean see = true;
+		boolean learn = true;
 
-	if ( see && learn ){
-		System.out.println ( "true" );
+		if ( see && learn ){
+			System.out.println ( "true" );
+		}
+
+		if ( !see || learn ){
+			System.out.println( "true" );
+		}*/
+
+
+
+		/*if(  ( false || true ) && true ){
+			System.out.println("Or Succesful");
+		}
+
+		if ( false ){
+			System.out.println("And Succesful");
+		}
+
+		if ( (false||true) && false || false || false  ){
+			System.out.println("anal leakage");
+		}
+
+		if(  ( false && false ) || true ){
+				System.out.println("Searching for");
+		}*/
+
 	}
-
-	if ( !see || learn ){
-		System.out.println( "true" );
-	}*/
-
-
-
-	/*if(  ( false || true ) && true ){
-		System.out.println("Or Succesful");
-	}
-
-	if ( false ){
-		System.out.println("And Succesful");
-	}
-
-	if ( (false||true) && false || false || false  ){
-		System.out.println("anal leakage");
-	}
-
-	if(  ( false && false ) || true ){
-			System.out.println("Searching for");
-	}*/
-
-}
 
 }
 
