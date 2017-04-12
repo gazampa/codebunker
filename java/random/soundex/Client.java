4812dfd2a214bc5fd6d8418fdf3dd5f77f076167
Billy Murphy <billy.1.murphy@gmail.com>
2017-04-12 13:45:10 -0500

    add description

--- java/random/soundex/Client.java
+++ java/random/soundex/Client.java
@@ -3,6 +3,20 @@ package org.gazampa.soundex;
 import java.util.ArrayList;
 import org.gazampa.soundex.Singleton;
 
+ /**	https://en.wikipedia.org/wiki/Soundex
+
+ 	Soundex is a phonetic algorithm for indexing names by sound, as pronounced
+ 	in English. Soundex was developed by Robert C. Russell and Margaret
+ 	King[8] Odell and patented in 1918[9] and 1922.	The Soundex code for a name
+ 	consists of a letter followed by three numerical digits: the letter is the
+ 	first letter of the name, and the digits encode the remaining consonants.
+ 	Consonants at a similar place of articulation share the same digit so, for
+ 	example, the labial consonants B, F, P, and V are each encoded as the
+ 	number 1.
+
+ **/
+
+
 public class Client{
 
 	public static void main(String[] args){
@@ -10,7 +24,7 @@ public class Client{
 	    String searchField = new String("class");
 	    String pattern = Singleton.SOUNDEX.getCodecVersion1_6().soundex(searchField);
 		ArrayList<String> listOfTerms = new ArrayList<String>(); // make it a object with string fields
-		listOfTerms.add("classs");listOfTerms.add("clazz");listOfTerms.add("something");listOfTerms.add("anotherthing");
+		listOfTerms.add("classs");listOfTerms.add("clazz");listOfTerms.add("klass");listOfTerms.add("something");listOfTerms.add("anotherthing");
 		ArrayList<String> results = new ArrayList<String>();
 
 		System.out.println("find terms similiar to " + searchField + " in the following list" + listOfTerms.toString());
