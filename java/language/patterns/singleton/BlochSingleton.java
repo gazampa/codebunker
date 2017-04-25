8242b1a849725e0ad5910f166a42dd518d6241a5
Billy Murphy <billy.1.murphy@gmail.com>
2017-04-24 19:18:52 -0500

    illustrate ability to overwrite values in Client.java

--- java/language/patterns/singleton/BlochSingleton.java
+++ java/language/patterns/singleton/BlochSingleton.java
@@ -8,6 +8,7 @@ public enum BlochSingleton{
 
 	private final int[] numbers = {0,1,2,3,4,5,6,7,8,9};
 
+	// see Client.java : you can overwrite values in this array in the client as only the reference is final
 	public int[] getNumbers(){
 		return numbers;
 	}
