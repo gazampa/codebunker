ab77975821ed5d29bb6660a447f3eb3245ccee18
Billy Murphy <billy.1.murphy@gmail.com>
2017-04-29 17:12:25 -0500

    encoding UTF-8

--- java/random/MakeAHashOfThings.java
+++ java/random/MakeAHashOfThings.java
@@ -1,9 +1,11 @@
 package org.gazampa.random;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.Security;
+import org.apache.commons.codec.binary.Base64;
 
 class MakeAHashOfThings{
 
@@ -18,9 +20,11 @@ class MakeAHashOfThings{
 
 		try{
 			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-			messageDigest.update(stringToEncrypt.getBytes());
-			String encryptedString = new String(messageDigest.digest());
+			messageDigest.update(stringToEncrypt.getBytes("UTF-8"));
+			String encryptedString = new String(messageDigest.digest(), "UTF-8");
 			System.out.println(encryptedString.hashCode());
+		}catch(UnsupportedEncodingException ex){
+			System.out.println("Ooops, I dont know that encoding " + ex.toString());
 		}catch(NoSuchAlgorithmException ex){
 			System.out.println("Ooops, no such algorithm. the following are provided");
 			System.out.println();
