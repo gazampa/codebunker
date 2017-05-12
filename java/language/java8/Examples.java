0bb1587ef0abaa1d788173c00c978259f4f687b6
Billy Murphy <billy.1.murphy@gmail.com>
2017-05-11 20:35:55 -0500

    add streams examples

--- java/language/java8/Examples.java
+++ java/language/java8/Examples.java
@@ -1,17 +1,74 @@
 package java8;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java8.interfaces.Interface;
 import java8.interfaces.InterfaceWithMethodImplementations;
 import java8.lambda.Lambda;
-import org.gazampa.stopwatch.StopWatch;
+import java8.streams.Streams;
+import org.gazampa.stopwatch.StopWatch; // jmh provides more rigorous benchmarking
 class Examples{
 
-	/** some examples taken from the JournalDev magazine **/
+	/** some examples based on article in JournalDev magazine : http://www.journaldev.com/2774/java-8-stream **/
 
 	public static void main(String[] args){
 
+		/** Stream Examples **/
+
+		Streams s = new Streams();
+		ArrayList<Integer> list = s.populateList(100);
+		int[] array = s.populateArray(100);
+		ArrayList<Integer> resultList;
+		int[] resultArray;
+		StopWatch sw = new StopWatch();
+
+		sw.start();
+		resultList = s.filterList(list, 500);
+		sw.stopAndReport();
+		System.out.println(resultList.toString());
+
+		sw.restart();
+		resultArray = s.filterArray(array, 500);
+		sw.stopAndReport();
+		System.out.println(Arrays.toString(resultArray));
+
+		sw.restart();
+		resultList = s.mapList(list);
+		sw.stopAndReport();
+		System.out.println(resultList.toString());
+
+		sw.restart();
+		resultArray = s.mapArray(array);
+		sw.stopAndReport();
+		System.out.println(Arrays.toString(array));
+
+		sw.restart();
+		resultList = s.sortList(list);
+		sw.stopAndReport();
+		System.out.println(resultList.toString());
+
+		sw.restart();
+		resultArray = s.sortArray(array);
+		sw.stopAndReport();
+		System.out.println(Arrays.toString(array));
+
+	}
+
+}
+
+/** Interface Examples
+	Interface iface = new Interface();
+
+	System.out.println(iface.interfaceMethod());
+	System.out.println(iface.staticsMethod());
+	System.out.println(iface.imLikeAbstractInherited());
+	// you have to make the interface public
+	System.out.println(InterfaceWithMethodImplementations.iCantBeOverridden());
+
+**/
+
+/** Lambda Examples
 	Lambda lamda = new Lambda();
 	StopWatch sw = new StopWatch();
 
@@ -41,19 +98,4 @@ class Examples{
 	sw.restart();
 	System.out.println(lamda.iterator_4filters());
 	sw.stopAndReport("iterator");
-
-
-	}
-
-}
-
-/** Interface Examples
-	Interface iface = new Interface();
-
-	System.out.println(iface.interfaceMethod());
-	System.out.println(iface.staticsMethod());
-	System.out.println(iface.imLikeAbstractInherited());
-	// you have to make the interface public
-	System.out.println(InterfaceWithMethodImplementations.iCantBeOverridden());
-
 **/
