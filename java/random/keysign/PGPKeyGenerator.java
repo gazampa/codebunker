c3730736ff6a8bc521d1b6d75a99bc8fd14ba908
Billy Murphy <billy.1.murphy@gmail.com>
2017-06-01 19:16:19 -0500

    remove no transfer files

--- java/random/keysign/PGPKeyGenerator.java
+++ java/random/keysign/PGPKeyGenerator.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 import java.util.Base64;
 import org.bouncycastle.openpgp.PGPKeyPair;
 import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.PGPPrivateKey;
 import org.bouncycastle.openpgp.PGPSecretKey;
 import org.bouncycastle.openpgp.PGPUtil;
 import org.bouncycastle.openpgp.PGPException;
@@ -50,38 +51,39 @@ public class PGPKeyGenerator{
 
 			PGPKeyPair keyPairPGP = ex.getPGPKeyPair();
 
+			PGPPrivateKey priv = ex.getPrivateKey();
+
 			PGPPublicKey pub = ex.getPublicKey();
 
 			PGPSecretKey secret = ex.createPGPSecretKey();
 
 			//Returns the key in its primary encoding format,
-			byte[] secretBytes = secret.getEncoded();
+			byte[] privBytes = keyPairPGP.getPrivateKey().getPrivateKeyDataPacket().getEncoded();
 			byte[] pubBytes = pub.getEncoded();
+			byte[] secretBytes = secret.getEncoded();
 
 			// generate a base64 encoding and write each to files
-			String secretB64Enc = Base64.getEncoder().encodeToString(secretBytes);
+			String privB64Enc = Base64.getEncoder().encodeToString(privBytes);
 			String pubB64Enc = Base64.getEncoder().encodeToString(pubBytes);
+			String secretB64Enc = Base64.getEncoder().encodeToString(secretBytes);
 
 			System.out.println(Arrays.toString(secretBytes));
 			System.out.println(Arrays.toString(pubBytes));
+			System.out.println(Arrays.toString(privBytes));
 
 			System.out.println(secretB64Enc);
 			System.out.println(pubB64Enc);
-
+			System.out.println(privB64Enc);
 
             FileOutputStream out1 = new FileOutputStream("pgp-secret-2048.asc");
-            FileOutputStream out2 = new FileOutputStream("pgp-pub-2048-no-transfer.asc");
+            FileOutputStream out2 = new FileOutputStream("pgp-pub-2048.asc");
             FileOutputStream out3 = new FileOutputStream("pgp-secret-2048.bpg");
-            FileOutputStream out4 = new FileOutputStream("pgp-pub-2048-no-transfer.bpg");
-            FileOutputStream out5 = new FileOutputStream("pgp-pub-2048-for-transfer.asc");
-            FileOutputStream out6 = new FileOutputStream("pgp-pub-2048-for-transfer.bpg");
+            FileOutputStream out4 = new FileOutputStream("pgp-pub-2048.bpg");
 
 			ex.writeSecretArmored(out1, secret);
 			ex.writeSecretStandard(out3, secret);
 			ex.writePublicArmored(out2, pub);
 			ex.writePublicStandard(out4, pub);
-			ex.writePublicArmoredForTransfer(out5, pub);
-			ex.writePublicStandardForTransfer(out6, pub);
 
 		}catch(Exception e){
 			System.out.println("The writer is unable to write " + e);
