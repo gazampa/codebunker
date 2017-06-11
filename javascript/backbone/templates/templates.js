ff66fd4ce3eabbf6fc3440033ff413c43cf875af
Billy Murphy <billy.1.murphy@gmail.com>
2017-06-11 14:00:15 -0500

    add last_checked_date field

--- javascript/backbone/templates/templates.js
+++ javascript/backbone/templates/templates.js
@@ -1,8 +1,20 @@
 Templates = {};
 
 Templates.collectionTemplate = [
-    "stations with LPG : " ,
+
+    "<h4>A Page of 100 Electric Car Charging Port Locations in Chicago : </h4>" ,
         "<% _.each(collection, function(model){ %>" ,
-            "<p><%=model.station_name %><br><%=model.street_address %><br><%=model.zip %><br><%=model.access_days_time %><br><%=model.station_phone %><br><%=model.longitude %> , <%=model.latitude %><br><%=model.cards_accepted %><br><%=model.fuel_type_code %></p>" ,
+            "<p>" ,
+            "<%=model.station_name %>" ,
+            "<br><%=model.street_address %>" ,
+            "<br><%=model.zip %>" ,
+            "<br><%=model.access_days_time %>" ,
+            "<br><%=model.station_phone %>" ,
+            "<br><%=model.longitude %> , <%=model.latitude %>" ,
+            "<br><%=model.cards_accepted %>" ,
+            "<br><%=model.fuel_type_code %>" ,
+            "<br>date last checked : <%=model.date_last_confirmed %>" ,
+            "</p>" ,
          "<% }); %>"
+
 ].join("\n").toString();
