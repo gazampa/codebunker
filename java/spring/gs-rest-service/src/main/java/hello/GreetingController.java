03107e2e26a8d992ebc93dca1517ece18999a471
Billy Murphy <billy.1.murphy@gmail.com>
2018-06-29 13:55:03 -0500

    spring examples

---
+++ java/spring/gs-rest-service/src/main/java/hello/GreetingController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value="/greeting", method={RequestMethod.POST,RequestMethod.GET}, produces={"application/json"}, consumes="text/html")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
