0db6960aefe511f3fd68824695b77d5393221f70
Billy Murphy <billy.1.murphy@gmail.com>
2024-07-16 17:10:29 -0500

    rm random folder from git

--- java/random/TestClass.java
+++
@@ -1,7 +0,0 @@
-class TestClass {
-
-	public static void main(String[] args){
-		System.out.println("this is a main method class");
-	}
-
-}
--- java/random/trustwave/barista/src/CoffeeIngredients.java
+++
@@ -1,9 +0,0 @@
-import java.util.Collection;
-import java.util.ArrayList;
-public class CoffeeIngredients{
-
-	Collection<Ingredient> coffeeIngredients = new ArrayList<Ingredient>();
-	private String name;
-
-
-}
