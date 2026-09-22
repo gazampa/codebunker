be9150b12de9ec1263ee1ec47c78ce028e557a71
gazampa <billy.murphy.work@gmail.com>
2026-09-22 15:05:36 -0500

    security sweep: Uncontrolled data used in path : added a method to force path.resolve

--- javascript/nodejs/server_with_express.js
+++ javascript/nodejs/server_with_express.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var fs = require('fs');
+const BASE_DIR = path.resolve(__dirname);
 var RateLimit = require('express-rate-limit');
 var limiter = RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -32,19 +33,25 @@ app.use(function (req, res, next) {
 
 app.get('/getLiverpool', function (req, res) {
         console.log( 'request from : ' +  ' : ' + req.headers.origin + ' : ' + req.originalUrl );
-        fs.readFile( __dirname + '/json/' + 'liverpool.jsonarray', 'utf8', function (err, data) {
+        var path = '/json/' + 'liverpool.jsonarray'; 
+        var safePath = getSafePath(BASE_DIR, path);
+        fs.readFile( safePath, 'utf8', function (err, data) {
         res.end( data );
    });
 })
 app.get('/getChicago_Fire', function (req, res) {
         console.log( 'request from : ' +  ' : ' + req.headers.origin + ' : ' + req.originalUrl );
-        fs.readFile( __dirname + '/json/' + 'chicago_fire.json', 'utf8', function (err, data) {
+        var path = '/json/' + 'chicago_fire.json';
+        var safePath = getSafePath(BASE_DIR, path);
+        fs.readFile( safePath, 'utf8', function (err, data) {
         res.end( data );
    });
 })
 app.get('/getDublin', function (req, res) {
         console.log( 'request from : ' +  ' : ' + req.headers.origin + ' : ' + req.originalUrl );
-        fs.readFile( __dirname + '/json/' + 'dublin.json', 'utf8', function (err, data) {
+        var path = '/json/' + 'dublin.json';
+        var safePath = getSafePath(BASE_DIR, path);        
+        fs.readFile( safePath , 'utf8', function (err, data) {
         res.end( data );
    });
 })
@@ -52,7 +59,9 @@ app.post('/postTeam', urlencodedParser, function (req, res) {
         // Prepare output in JSON format
         console.log( 'post request from : ' +  ' : ' + req.headers.origin + ' : ' + req.originalUrl );
         console.log( 'for : ' +  req.body.team);
-        fs.readFile( __dirname + '/json/' + req.body.team + '.json', 'utf8', function (err, data) {
+        var path = '/json/' + req.body.team + '.json';
+        var safePath = getSafePath(BASE_DIR, path);        
+        fs.readFile( safePath, 'utf8', function (err, data) {
         res.end( data );
     });
 })
@@ -66,3 +75,21 @@ var server = app.listen(8080, function () {
   console.log('nodejs listening at http://%s:%s', host, port)
 
 })
+
+function getSafePath(baseDir, pathInput) {
+   
+    // 1. Reject bad characters
+    if (pathInput.includes('\0')) {
+        throw new Error('Invalid path characters');
+    }    
+
+    // 2. Resolve to an absolute path
+    const safePath = path.resolve(baseDir, relativeInput);
+
+    // 3. Verify the path stays inside the base directory
+    if (!safePath.startsWith(path.resolve(baseDir))) {
+        throw new Error('Access Denied: Invalid path');
+    }
+
+    return safePath;
+}
